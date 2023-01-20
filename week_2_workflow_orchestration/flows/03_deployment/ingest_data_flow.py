d201edb2f779046ab2fd7d0da2f01c5e156506b5
khoinguyen19k8 <khoinguyen19k8@gmail.com>
2023-01-20 18:17:00 +0100

    Add parameterized_flows

---
+++ week_2_workflow_orchestration/flows/03_deployment/ingest_data_flow.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import argparse
+
+from time import time
+
+import pandas as pd
+from sqlalchemy import create_engine
+from prefect import flow, task
+from prefect.tasks import task_input_hash
+from datetime import timedelta
+from prefect_sqlalchemy import SqlAlchemyConnector
+
+
+@task(
+    log_prints=True,
+    tags=["extract"],
+    cache_key_fn=task_input_hash,
+    cache_expiration=timedelta(days=1),
+)
+def extract_data(url: str):
+    # the backup files are gzipped, and it's important to keep the correct extension
+    # for pandas to be able to open the file
+    if url.endswith(".csv.gz"):
+        csv_name = "output.csv.gz"
+    else:
+        csv_name = "output.csv"
+
+    os.system(f"wget {url} -O {csv_name}")
+
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    return df
+
+
+@task(log_prints=True)
+def transform_data(df):
+    print(f"pre: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+    df = df.loc[df["passenger_count"] != 0]
+    print(f"post: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+    return df
+
+
+@task(log_prints=True, retries=3)
+def load_data(table_name, df):
+    connection_block = SqlAlchemyConnector.load("postgres-connector")
+    with connection_block.get_connection(begin=False) as engine:
+        df.head(n=0).to_sql(name=table_name, con=engine, if_exists="replace")
+
+        df.to_sql(name=table_name, con=engine, if_exists="append")
+
+    # while True:
+
+    #     try:
+    #         t_start = time()
+    #
+    #         df = next(df_iter)
+
+    #         df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    #         df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    #         df.to_sql(name=table_name, con=engine, if_exists='append')
+
+    #         t_end = time()
+
+    #         print('inserted another chunk, took %.3f second' % (t_end - t_start))
+
+    #     except StopIteration:
+    #         print("Finished ingesting data into the postgres database")
+    #         break
+
+
+@flow(name="Subflow", log_prints=True)
+def log_subflow(table_name: str):
+    print(f"Logging Subflow for : {table_name}")
+
+
+@flow(name="Ingest Data")
+def main_flow():
+    parser = argparse.ArgumentParser(description="Ingest CSV data to Postgres")
+
+    parser.add_argument(
+        "--table_name",
+        required=True,
+        help="name of the table where we will write the results to",
+    )
+
+    parser.add_argument("--url", required=True, help="url of the csv file")
+
+    args = parser.parse_args()
+
+    log_subflow(args.table_name)
+    raw_data = extract_data(args.url)
+    data = transform_data(raw_data)
+    load_data(args.table_name, data)
+
+
+if __name__ == "__main__":
+    main_flow()
