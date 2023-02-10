8e81f1b5f068e7f0ca2794e8fa9bf1a8e8f7f541
khoinguyen19k8 <khoinguyen19k8@gmail.com>
2023-02-10 15:22:38 +0100

    Add flow code for hw week2

---
+++ week_2_workflow_orchestration/flows/04_hw/etl_gcs_to_bq.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from prefect_gcp import GcpCredentials
+
+
+@task(retries=3)
+def extract_from_gcs(color: str, year: int, month: int) -> Path:
+    """Download trip data from GCS"""
+    gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
+    gcs_block = GcsBucket.load("zoom-gcs")
+    gcs_block.get_directory(from_path=gcs_path, local_path="./")
+    return Path(f"./{gcs_path}")
+
+
+@task()
+def transform(path: Path) -> pd.DataFrame:
+    """Data cleaning example"""
+    df = pd.read_parquet(path)
+    # print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
+    # df["passenger_count"] = df["passenger_count"].fillna(0)
+    # print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
+    return df
+
+
+@task()
+def write_bq(df: pd.DataFrame) -> None:
+    """Write DataFrame to BigQuery"""
+    gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
+
+    df.to_gbq(
+        destination_table="dezoomcamp.rides",
+        project_id="dtc-de-372302",
+        credentials=gcp_credentials_block.get_credentials_from_service_account(),
+        chunksize=500_000,
+        if_exists="append",
+    )
+
+
+@flow(log_prints=True)
+def etl_gcs_to_bq(months: list[int], year: int, color: str):
+    """Main ETL flow to load data into Big Query"""
+
+    for month in months:
+        path = extract_from_gcs(color, year, month)
+        df = transform(path)
+        print(f"rows: {len(df)}")
+        write_bq(df)
+
+
+if __name__ == "__main__":
+    color = "yellow"
+    months = [1, 2, 3]
+    year = 2021
+    etl_gcs_to_bq(months, year, color)
---
+++ week_2_workflow_orchestration/homework.md
@@ -0,0 +1,17 @@
+# Question 1
+
+* 447,770
+
+# Question 2
+
+* 0 5 1 * *
+
+# Question 3
+
+* 14,851,920
+
+# Question 4
+
+# Question 5
+
+# Question 6
