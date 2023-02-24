0a6e0d6577e42ddbf73fbb7b20a5e604c55e3f8b
khoinguyen19k8 <khoinguyen19k8@gmail.com>
2023-02-24 16:20:17 +0100

    Preparing Yellow and Green Taxi Data

---
+++ week_5_batch_processing/download_data.sh
@@ -0,0 +1,25 @@
+dataset_file = f"{color}_tripdata_{year}-{month:02}"
+dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+set -e
+
+TAXI_TYPE=$1 # "yellow"
+YEAR=$2 # 2020
+
+URL_PREFIX="https://github.com/DataTalksClub/nyc-tlc-data/releases/download"
+
+for MONTH in {1..12}; do
+  FMONTH=`printf "%02d" ${MONTH}`
+
+  URL="${URL_PREFIX}/${TAXI_TYPE}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv.gz"
+
+  LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
+  LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv.gz"
+  LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
+
+  echo "donwloading ${URL} to ${LOCAL_PATH}"
+  mkdir -p ${LOCAL_PREFIX}
+  wget ${URL} -O ${LOCAL_PATH}
+
+  #echo "decompressing ${LOCAL_PATH}"
+  #gzip -d ${LOCAL_PATH}
+done
